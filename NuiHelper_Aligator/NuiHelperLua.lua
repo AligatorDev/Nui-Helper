1d29eb7b7cc5db4e83f50a2836e52b9e2e7b8a90
Aligator Dev <28674704+AligatorDev@users.noreply.github.com>
2021-01-15 15:34:12 -0300

    Add files via upload

---
+++ NuiHelper_Aligator/NuiHelperLua.lua
@@ -0,0 +1,58 @@
+
+Exports = {}
+
+local function UpdateResourceName()
+    SendNuiMessage({
+        internal = true,
+        var = "resourceName",
+        set = GetCurrentResourceName()
+    })  
+end
+local funcs = {}
+function Exports:SetTable(ftable) 
+    funcs = ftable
+  
+end
+
+
+
+ function Exports:Call(name,...) 
+    callbackid = GetGameTimer()
+    SendNuiMessage(json.encode({
+        internal = false,
+        func = name,
+        resname = GetCurrentResourceName(),
+        callbackid = callbackid,
+        args = json.encode({...});
+    }))  
+    local retorno = nil
+    AddCallBack(callbackid,function(...)
+        retorno = ...;
+    end)
+    while  retorno == nil do
+        Citizen.Wait(10)
+    end
+    return retorno;
+end
+local clbs = {}
+
+
+local function AddCallBack(cb,calb)
+    clbs[cb] = calb;
+
+end
+RegisterNUICallback('CALLBACKS', function(data, cb)
+    local f =  clbs[data.callbackid]
+    if f then
+        f(data.retorno)
+        clbs[data.callbackid] = nil;
+    end
+    cb("ok")
+end)
+RegisterNUICallback('func', function(data, cb)
+    local f =  funcs[data.func]
+    if f then
+        cb(json.encode(f(table.unpack(data.args))))
+    end
+end)
+
